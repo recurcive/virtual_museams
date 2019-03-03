24f057734b7151481247982cae938631743a9a1d
Dmitry <recurcive@gmail.com>
2019-03-03 16:58:12 +0300

    Added some regions: spb, pskov, irkutsk

--- src/Region.js
+++ src/Region.js
@@ -7,7 +7,9 @@ import axios from 'axios';
 
 const regionName = {
   kr: 'Красноярский край',
-
+  ir: 'Иркутская область',
+  le : 'Санкт-Петербург и Ленинградская область',
+  ps: 'Псковская область'
 };
 
 class Region extends Component {
@@ -32,6 +34,10 @@ class Region extends Component {
         .then(response => {return  response.json()})
         .then(data => this.setState({ text: data }))*/
 
+//        this.setState( { data: [] } );
+
+//        console.log(`/regions/${params.code}/text_${params.code}.json`);
+
 
         axios.get(`/regions/${params.code}/text_${params.code}.json`)
            .then( response => {
@@ -39,7 +45,6 @@ class Region extends Component {
              data: response.data
            });
          })
-
 
     }
 
@@ -51,12 +56,30 @@ class Region extends Component {
                       "age":30,
 
                     };
+                    const styles = {
+
+                      content: {
+                        height: '100%',
+                        width: '100%',
+                        backgroundColor: 'rgba(255, 0, 0, 0.1)'
+                      }
+                    }
+
+
 
     const region = this.state.data;
+    const imgUrl = `/regions/${params.code}/back_${params.code}.jpg`;
 
     return (
 
-        <div className="App">
+        <div className="App"
+        style = {{ backgroundImage: 'url(' + imgUrl +')',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center center',
+                        backgroundRepeat: 'no-repeat',
+
+                      }}>
+            <div style={styles.content}>
             <h1>{ regionName[params.code] }</h1>
             <hr align="center" color="Red" />
 
@@ -81,12 +104,13 @@ class Region extends Component {
 
               <Row className="show-grid text-justify">
               <Col>
-                    { region.museams.map(info => ( <ul><h4>{info.city}</h4>{info.list.map(museam => <li>{museam}</li>)} </ul>))}
+                    { region.museams.map(info => ( <ul><h4>{info.city}</h4>{info.list.map(museam => <li>{<a href={museam.link} target='_blank'>{museam.name}</a>}</li>)} </ul>))}
               </Col>
               </Row>
 
 
             </Grid>
+            </div>
 
         </div>
 
